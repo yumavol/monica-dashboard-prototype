0a461f9fa47799aa21c0fe5e5ddb524a08ee1ea6
yumavol <yumavol@gmail.com>
2020-05-20 12:57:09 +0700

    Fix homepage

---
+++ precache-manifest.e84a9f23ad1678fe3315d8fb05e545fc.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4495be4a3132aae851e5702b371c8b51",
+    "url": "/monica-dashboard-prototype/index.html"
+  },
+  {
+    "revision": "241c292bbdb04c8dab74",
+    "url": "/monica-dashboard-prototype/static/js/0.21cccbd0.chunk.js"
+  },
+  {
+    "revision": "e3bca9939337fca8fa9a",
+    "url": "/monica-dashboard-prototype/static/js/10.80e7130a.chunk.js"
+  },
+  {
+    "revision": "30d96d489da187917db5",
+    "url": "/monica-dashboard-prototype/static/js/3.f3e79cc5.chunk.js"
+  },
+  {
+    "revision": "e7cad5e8c80a4a756b57f883a8b131b2",
+    "url": "/monica-dashboard-prototype/static/js/3.f3e79cc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15d788ac2b7a7b4973e5",
+    "url": "/monica-dashboard-prototype/static/js/4.5598e2a3.chunk.js"
+  },
+  {
+    "revision": "d9099e2f84d4b7eccc8dd2b72ce6e468",
+    "url": "/monica-dashboard-prototype/static/js/4.5598e2a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59b3bf08c4dd67c53f68",
+    "url": "/monica-dashboard-prototype/static/js/5.f1d59619.chunk.js"
+  },
+  {
+    "revision": "9be83adfd9fbde18af16",
+    "url": "/monica-dashboard-prototype/static/js/6.21b69711.chunk.js"
+  },
+  {
+    "revision": "4a43e21624051156b48f",
+    "url": "/monica-dashboard-prototype/static/js/7.89b36fde.chunk.js"
+  },
+  {
+    "revision": "03bb1512d92a2295a70b",
+    "url": "/monica-dashboard-prototype/static/js/8.4d2adfb4.chunk.js"
+  },
+  {
+    "revision": "d5351f883a68987fa7a6",
+    "url": "/monica-dashboard-prototype/static/js/9.10dfd32b.chunk.js"
+  },
+  {
+    "revision": "b7f419995de8bc12c685",
+    "url": "/monica-dashboard-prototype/static/js/main.341a6906.chunk.js"
+  },
+  {
+    "revision": "9a5fbe37d34419f03515",
+    "url": "/monica-dashboard-prototype/static/js/runtime-main.a58a6515.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.47c220f1ce390889836141a76b58cbc4.js"
+  "/monica-dashboard-prototype/precache-manifest.e84a9f23ad1678fe3315d8fb05e545fc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/monica-dashboard-prototype/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
